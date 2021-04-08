6099164ec70b2d7d888644070efcb59b31504538
magdalena-o <magdalena.obermayr@gmx.at>
2021-04-08 21:04:19 +0200

    implement design htmlquiz landing page

---
+++ game.js
@@ -0,0 +1,10 @@
+function updateHTMLQuiz() {
+    document.getElementById('quiz').innerHTML += `
+    <div class="bg-img">
+        <img src="img/bg b.png"> 
+    <div>
+    <div class="html-quiz">
+     <p><b>Welcome to <br> The Awesome HTML Quiz</b></p>
+    </div>
+    `;
+}
